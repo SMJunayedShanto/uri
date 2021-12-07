5f62116df2f6b36a8ea93fa1249baa2bafff115d
SMJunayedShanto <smjunayedshanto@gmail.com>
2021-12-07 21:32:47 +0600

    2nd problem solved

---
+++ URI_Problem/src/main/java/problem/ExtremelyBasic1001.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.Scanner;
+
+public class ExtremelyBasic1001 {
+
+	public static void main(String argd[]) {
+		int a,b;
+	    
+	    Scanner scan = new Scanner(System.in);
+	    
+	    a = scan.nextInt();
+	    b = scan.nextInt();
+	    
+	    System.out.println("X = "+(a+b));
+	}
+}
